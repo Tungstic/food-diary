170a4732f56693b30739a312aae6f0a8db2dacec
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-17 12:47:34 +0200

    Add calendar to HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,10 @@
+import MyCalendar from './components/Calendar';
+
 export default function HomePage() {
-  return <div>Welcome to my home page</div>;
+  return (
+    <>
+      <div>Welcome to my home page</div>
+      <MyCalendar />
+    </>
+  );
 }
