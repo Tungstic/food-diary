55230543a59c13d940a510b5a1dca8abbea33e29
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-17 16:46:50 +0200

    Style the calendar

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -1,18 +1,11 @@
 'use client';
 
-import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 
 export default function MyCalendar() {
   const [value, setValue] = useState(new Date());
 
-  return (
-    <Calendar
-   
-      onChange={setValue}
-      value={value}
-      locale="en-GB"
-    />
-  );
+  return <Calendar onChange={setValue} value={value} locale="en-GB" />;
 }
