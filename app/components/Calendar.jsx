b79ac81608fe0c782894d4a72c963ea3d9f6c0ae
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-19 12:37:14 +0200

    Add comment to Calendar.jsx for onClickDay events later

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -7,5 +7,12 @@ import Calendar from 'react-calendar';
 export default function MyCalendar() {
   const [value, setValue] = useState(new Date());
 
-  return <Calendar onChange={setValue} value={value} locale="en-GB" />;
+  return (
+    <Calendar
+      // onClickDay={(event) => alert('Hello')} - attach my fn for redirect to entries on that day
+      onChange={setValue}
+      value={value}
+      locale="en-GB"
+    />
+  );
 }
