e98d433dd1bbfebed1a8bc1dd0f888b69d3763d6
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-29 10:25:01 +0200

    Display today's meal names on HomePage

--- app/TodaysEntries.jsx
+++ app/TodaysEntries.jsx
@@ -1,29 +1,33 @@
-export default async function TodaysEntries() {
-  const listOfEntries = [];
+'use client';
 
-  async function getTodaysEntries() {
-    const response = await fetch('http://localhost:3000/api/entries');
-    const data = await response.json();
-    console.log(data);
-    listOfEntries.push(data.entries);
-  }
+import { useEffect, useState } from 'react';
 
-  getTodaysEntries().catch((error) => {
-    console.log(error);
-  });
+export default function TodaysEntries() {
+  const [listOfEntries, setListOfEntries] = useState([]);
 
-  await getTodaysEntries();
-  return (
-    <>
-      <div>Your entries today</div>
-      <div>{JSON.stringify(listOfEntries)}</div>
-      {listOfEntries.length > 0 && (
+  useEffect(() => {
+    async function getTodaysEntries() {
+      const response = await fetch('http://localhost:3000/api/entries');
+      const data = await response.json();
+      console.log(data);
+      setListOfEntries(data.entries);
+    }
+
+    getTodaysEntries().catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+  if (listOfEntries.length > 0) {
+    return (
+      <div>
+        <div>The meals you logged today</div>
         <div>
           {listOfEntries.map((entry) => {
-            return <div key={`entry ${entry.mealName}`}>{entry.mealName}</div>;
+            return <div key={`meal number ${entry.id}`}>{entry.mealName}</div>;
           })}
         </div>
-      )}
-    </>
-  );
+      </div>
+    );
+  }
 }
