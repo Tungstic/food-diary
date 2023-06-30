60ec8d6f74cdbe80ca6011a690fe94b6c95dfb13
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-30 11:31:39 +0200

    Log the clicked date from Calendar

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -7,15 +7,17 @@ import Calendar from 'react-calendar';
 export default function MyCalendar() {
   const [value, setValue] = useState(new Date());
 
-  async function getEntriesByDate(date, userId) {
+  /* async function getEntriesByDate(date, userId) {
     const response = await fetch(`http://localhost:3000/api/entries/${date}`);
-  }
+  } */
 
   return (
     <Calendar
-      onClickDay={() => alert('Hello')}
-      onChange={setValue}
       value={value}
+      onChange={(value) => {
+        setValue(value);
+        console.log(value);
+      }}
       locale="en-GB"
     />
   );
