26999088f928a47a655c42a58d2a1b442f119dc5
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-30 12:53:29 +0200

    Add dropdowns from DB to ProfilePage

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,11 @@
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import CreatableSelect from 'react-select/creatable';
+import { getAllIngredients } from '../../database/ingredients';
+import { getAllSymptoms } from '../../database/symptoms';
 import { getUserBySessionToken } from '../../database/users';
+import InputForm from './InputForm';
 
 export default async function ProfileUsernamePage() {
   // 1. get the session token from the cookie
@@ -15,10 +20,24 @@ export default async function ProfileUsernamePage() {
     notFound();
   }
 
+  const symptoms = await getAllSymptoms();
+  const ingredients = await getAllIngredients();
+
   return (
     <>
-      <div>id: {user.id}</div>
-      <div>username: {user.username}</div>
+      <div>{`Hello, ${user.username.at(0)?.toUpperCase()}${user.username.slice(
+        1,
+      )}`}</div>
+      <div>
+        This is your profile. Here you can see the full list of symptoms and
+        ingredients and add to it. You can also create new symptoms and/or
+        ingredients while making{' '}
+        <Link style={{ textDecoration: 'none', color: 'red' }} href="/new">
+          a new entry
+        </Link>
+      </div>
+
+      <InputForm symptoms={symptoms} user={user.id} ingredients={ingredients} />
     </>
   );
 }
